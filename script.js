a3b4c9738f236bc8f3798c72c1d9774a22fd28c7
Sujosh <sujoshk@gmail.com>
2020-10-10 02:12:58 +0530

    built modal

---
+++ script.js
@@ -0,0 +1,16 @@
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEl = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+// Show modal, focus on input
+function showModal() {
+  modal.classList.add('show-modal');
+  websiteNameEl.focus();
+}
+
+// Modal Event Listeners
+modalShow.addEventListener('click', showModal);
